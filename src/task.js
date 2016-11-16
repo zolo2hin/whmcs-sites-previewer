68c30712f061691406b0e96e6a7b2552c589ffec
Serjik <serjikz123@gmail.com>
2016-11-16 14:24:31 +0200

    Update default task
    * remove debugging
    * fill image white color by default

--- src/task.js
+++ src/task.js
@@ -7,18 +7,6 @@ var address = system.args[1],
 	b_height = system.args[4],
 	c_width = system.args[5],
 	c_height = system.args[6];
-	console.log('address');
-console.log(address);
-console.log('file');
-console.log(file);
-console.log('b_width');
-console.log(b_width);
-console.log('b_height');
-console.log(b_height);
-console.log('c_width');
-console.log(c_width);
-console.log('c_height');
-console.log(c_height);
 
 page.viewportSize = {width: b_width, height: b_height};
 
@@ -26,6 +14,13 @@ page.clipRect = {"width":c_width,"height":c_height,"top":0,"left":0};
 
 page.open(address, function (status) {
     if (status !== 'success') { phantom.exit(1); }
+    
+    page.evaluate(function() {
+      if (document && document.body) {
+          document.body.bgColor = '#FFFFFF';
+      }
+    });
+    
     page.render(file);
     phantom.exit();
 });
